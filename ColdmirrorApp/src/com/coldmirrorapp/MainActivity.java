be1857d4b9e4a5276028189477edcac95c65c79e
Klaus <dieechtenilente@users.noreply.github.com>
2016-12-03 21:46:18 +0100

    Add files via upload

---
+++ ColdmirrorApp/src/com/coldmirrorapp/MainActivity.java
@@ -0,0 +1,49 @@
+package com.coldmirrorapp;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	private MediaPlayer mediaPlayer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button stop = (Button) (findViewById(R.id.stop));
+		stop.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mediaPlayer != null) {
+					mediaPlayer.pause();
+					mediaPlayer.release();
+				}
+				mediaPlayer = null;
+			}
+		});
+
+		
+	}
+
+	public void onClick(View v) {
+		try {
+			int resId = getResources().getIdentifier(v.getTag() + "", "drawable", getPackageName());
+			mediaPlayer = MediaPlayer.create(this, resId);
+			mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+				public void onCompletion(MediaPlayer mp) {
+					mp.release();
+				};
+			});
+			mediaPlayer.start();
+
+		} catch (Exception e) {
+
+		}
+	}
+}
