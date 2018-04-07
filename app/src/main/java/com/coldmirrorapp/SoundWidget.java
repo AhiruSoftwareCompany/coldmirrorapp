b637d6c92e379b97ccbc8c7219dea30be7618023
Klaus <klaushackner@live.de>
2018-04-07 12:38:39 +0200

    Adds (work in progress) widget for playing random sound

--- app/src/main/java/com/coldmirrorapp/SoundWidget.java
+++ app/src/main/java/com/coldmirrorapp/SoundWidget.java
@@ -1,8 +1,11 @@
 package com.coldmirrorapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.widget.RemoteViews;
 
 /**
@@ -10,13 +13,19 @@ import android.widget.RemoteViews;
  */
 public class SoundWidget extends AppWidgetProvider {
 
+    public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.sound_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        System.out.println("hi");
+
+        Intent active = new Intent(context, SoundWidget.class);
+        active.setAction(ACTION_WIDGET_RECEIVER);
+        PendingIntent pI = PendingIntent.getBroadcast(context, 0, active, 0);
+        views.setOnClickPendingIntent(R.id.playSound, pI);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -28,8 +37,33 @@ public class SoundWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // v1.5 fix that doesn't call onDelete Action
+        final String action = intent.getAction();
+        if (AppWidgetManager.ACTION_APPWIDGET_DELETED.equals(action)) {
+            //The widget is being deleted off the desktop
+            final int appWidgetId = intent.getExtras().getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+            if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+                this.onDeleted(context, new int[]{appWidgetId});
+            }
+        } else {
+            // check, if our Action was called
+            if (intent.getAction().equals(ACTION_WIDGET_RECEIVER)) {
+                int resId = context.getResources().getIdentifier(Quote.getRandom().getId(), "raw", context.getPackageName());
+                MediaPlayer mPlay = MediaPlayer.create(context, resId);
+                mPlay.start();
+            } else {
+                // do nothing
+            }
 
 
+            super.onReceive(context, intent);
+        }
     }
 
     @Override
