2758be8dd5dc3626e55e87aafe68d33c8ae088b5
dieechtenilente <dieechtenilente@users.noreply.github.com>
2019-08-30 21:00:08 +0200

    Google Play Release 1667

--- app/src/main/java/com/coldmirrorapp/MainActivity.java
+++ app/src/main/java/com/coldmirrorapp/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         quoteArray = Quote.getAll();
 
         searchField = findViewById(R.id.searchField);
