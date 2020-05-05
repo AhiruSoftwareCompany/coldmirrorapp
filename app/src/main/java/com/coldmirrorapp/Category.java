231778bc019859ca81a7f4b4e4eb8fa4b9ca7edf
dieechtenilente <dieechtenilente@users.noreply.github.com>
2020-05-05 17:11:54 +0200

    Adds gettable names for categories

--- app/src/main/java/com/coldmirrorapp/Category.java
+++ app/src/main/java/com/coldmirrorapp/Category.java
@@ -2,12 +2,24 @@ package com.coldmirrorapp;
 
 enum Category {
 
-    harrypotter("#FF69B4"), avengers("#4286f4"), random("#F7B600"), japanoschlampen("#c141f4"), kunstwissenschaftlicheanalyse("#bab76a"), tatort("#0a1bb5"), nothing("");
+    harrypotter("", "#FF69B4"),
+    avengers("", "#4286f4"),
+    random("", "#F7B600"),
+    japanoschlampen("", "#c141f4"),
+    kunstwissenschaftlicheanalyse("", "#bab76a"),
+    tatort("", "#0a1bb5"),
+    nothing("", "");
 
+    String name;
     String color;
 
-    Category(String color) {
+    Category(String name, String color) {
+        this.name = name;
         this.color = color;
+    }
+
+    public String getName() {
+        return name;
     }
 
     @Override
